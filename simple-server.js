d1acf022af32bbe50b3c9e9684e9d03e4f1339d0
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2019-07-25 17:20:43 +0800

    added comments

--- simple-server.js
+++ simple-server.js
@@ -1,18 +1,23 @@
+// import express
 const express = require("express");
 
+// initialize server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// initialize route
 app.get("/", (req, res) => {
-  res.end("hello world"); // bare http method, will just send a string, without any headers set
-  //   res.send("<h1>hello world</h1>"); // express specific function, sets content type in header automatically (so this will be html)
+  // res.end("hello world"); // bare http method, will just send a string, without any headers set
+  res.send("<h1>hello world</h1>"); // express specific function, sets content type in header automatically (so this will be html)
 });
 
-app.get("/file/", (req, res) => {
+// route that will send the static index.html file
+app.get("/static/", (req, res) => {
   // manual way of sending static files
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// make server listen on port
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
