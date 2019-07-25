009c11f5b87ceb29caf2fc9cc4421bf71820d344
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2019-07-25 18:05:04 +0800

    commented code

--- simple-raw-graphql-server.js
+++ simple-raw-graphql-server.js
@@ -6,12 +6,15 @@ const {
   GraphQLString
 } = require("graphql");
 
+// schema definition
 let schema = new GraphQLSchema({
   query: new GraphQLObjectType({
+    // graphql always needs one root query
     name: "RootQueryType",
     fields: {
       welcome_message: {
         type: GraphQLString,
+        // The resolver defines what will be send back, once the field is queried
         resolve() {
           return "Welcome to my new GraphQL endpoint";
         }
@@ -20,14 +23,14 @@ let schema = new GraphQLSchema({
   })
 });
 
+// init express server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// the query is hardcoded for now
 app.get("/", (req, res) => {
+  // the query is hardcoded for now, we don't get it from the request
   let query = "{welcome_message}";
   graphql(schema, query).then(result => {
-    console.log(result);
     res.json(result);
   });
 });
