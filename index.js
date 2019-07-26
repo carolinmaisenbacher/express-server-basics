747ed9430cbd4d769d12c94cebc9394683abd956
carolinmaisenbacher <carolin.maisenbacher@code.berlin>
2019-07-26 14:47:57 +0800

    modified to use apollo-server

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 
-const logger = require("./middleware/logger");
+const animals = require("./assets/animals.json");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Init Middleware
-app.use(logger);
+const schema = gql`
+  type Query {
+    welcome_message: String
+    animal(name: String!): Animal
+    animals: [Animal]
+  }
+  type Animal {
+    name: String
+    description: String
+    color: String
+  }
+  input AnimalInput {
+    name: String!
+    description: String!
+    color: String
+  }
+  type Mutation {
+    addAnimal(animal: AnimalInput!): String
+  }
+`;
+const resolvers = {
+  Query: {
+    welcome_message: () => "Welcome to my first apollo server",
+    animal: name => animals[0],
+    animals: () => animals
+  },
+  Mutation: {
+    addAnimal: (_, animal) => {
+      console.log(animal);
+      return "success";
+    }
+  }
+};
 
-// Init Body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
 
-// Cookbook api routes
-app.use("/api/cookbooks", require("./routes/api/cookbooks"));
+server.applyMiddleware({ app, path: "/" });
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
